17db220730ef619eb7ceb32bf6c3045a71c07231
Mahdi A <81549809+Mahdi-Alizadeh96@users.noreply.github.com>
2021-08-08 23:28:21 +0430

    menu update

--- js/script.js
+++ js/script.js
@@ -9,8 +9,9 @@ $(document).ready(function () {
 
 const menuBox = document.querySelector('.nav-bar ul');
 
-
 const colexit = document.querySelector('.collpase-menu i');
+
+const backBlack = document.querySelector('.blackBack');
 
 
 
@@ -18,8 +19,16 @@ colexit.addEventListener('click', function() {
   if(this.classList.contains('fa-bars')) {
     this.classList = 'fas fa-times';
     menuBox.style.right = 0;
+    backBlack.style.display = 'block';
   }else {
-    this.classList = 'fas fa-bars'
-    menuBox.style.right = '-160px'
+    this.classList = 'fas fa-bars';
+    menuBox.style.right = '-160px';
+    backBlack.style.display = 'none';
   }
+})
+
+backBlack.addEventListener('click', () => {
+  colexit.classList = 'fas fa-bars';
+  menuBox.style.right = '-160px';
+  backBlack.style.display = 'none';
 })
